5d9a4a0f4c3369e013e141aa7804aa476019d41d
Aaron <ajones125@my.bcit.ca>
2019-01-14 15:03:12 -0800

    updated ajwhereis_check script

--- ajwhereis_check.sh
+++ ajwhereis_check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -
+#!/bin/bash
 #===============================================================================
 #
 #          FILE: whereis_check.sh
@@ -23,18 +23,19 @@ set -o nounset                              # Treat unset variables as an error
 #Variable declarations
 #todo place variable declarations here
 declare file 
-declare pathtofile
-
+declare whereis_output
+declare locations
+declare -i state_code=0
 #Solicit command that should be searched and store it in the varialbe command
 #todo: use read -p statement to get input from user and store it in the variable: cmd
-read -p "Please enter the file you would like to check where the file" \
-find or locate file
+read -p "Please enter the file you would like to check where the file: " \
+ file
 
 #Store the output of the whereis invocation that searches for the users inputed
 # command in the variable "whereis_output"
 #todo variable assigment to output of command - follows form variable=$( command )
 
-
+whereis_output=$(whereis $file)
 
 #Using the variable whereis_output process the output to drop the command name
 #store the output in locations
@@ -43,13 +44,11 @@ locations=$( (echo $whereis_output | cut -d ":" -f 2-) )
 if [[ "$locations" == "" ]]; then
   #the locations are empty i.e. command not found, tell the user
   #and set the exit code
-	read -p "Could not find the file you are looking for, was there a typo in the filename?"
-	$? = 1
+	echo "Could not find the cmd you are looking for, is there a typo?"
+	state_code=1
 else
-  #the locations contains the path to the command tell the user
+  #the locations contains the path to the command, tell the user
   #and set the exit code
-	echo "the path to the file is: $pathtofile"
-	$?=0
+	echo "the path to the file is: $locations"
 fi
-
-exit $pathtofile
+echo "The Status Code is: $state_code"
